d953c30a86d1709a7967489c68fb2c2b06971f47
Darien Miller <darienmiller88@yahoo.com>
2025-08-19 14:17:08 -0400

    feat: added templates folder and set up templates on back end

--- main.go
+++ main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main(){
-	fmt.Println("hello world")
-	
+	app := chi.NewRouter()
+	tmpl, err := template.ParseGlob("templates/*.html")
+
+	if err != nil{
+		panic(err)
+	}
+
+	app.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		err := tmpl.ExecuteTemplate(res, "index.html", nil)
+
+		if err != nil {
+			fmt.Println("err:", err)
+		}
+	})
+
+	fmt.Println("Listening on port 8080")
+	http.ListenAndServe(":8080", app)
 }
