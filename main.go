7f2da66e01943e9c7b123ba2b66418bb47a02af1
Darien Miller <darienmiller88@yahoo.com>
2025-08-20 16:36:57 -0400

    refactor: added centrifuge to frontend and backend

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/centrifugal/centrifuge"
 )
 
 func main(){
@@ -15,6 +16,14 @@ func main(){
 	if err != nil{
 		panic(err)
 	}
+
+	node, err := centrifuge.New(centrifuge.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(node.Config().Name)
 
 	app.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	app.Get("/", func(res http.ResponseWriter, req *http.Request) {
@@ -27,4 +36,20 @@ func main(){
 
 	fmt.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", app)
+}
+
+func auth(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		// Put authentication Credentials into request Context.
+		// Since we don't have any session backend here we simply
+		// set user ID as empty string. Users with empty ID called
+		// anonymous users, in real app you should decide whether
+		// anonymous users allowed to connect to your server or not.
+		credentials := &centrifuge.Credentials{
+			UserID: "",
+		}
+		newCtx := centrifuge.SetCredentials(ctx, credentials)
+		h.ServeHTTP(w, r.WithContext(newCtx))
+	})
 }
